edec6cf2ed7bca8de8e8b9318ed75de9545c2af7
pholance <pholance@gmail.com>
2014-10-27 11:57:09 +0800

    实现视频文件信息查询

--- src/main/java/com/yidumen/service/VideoService.java
+++ src/main/java/com/yidumen/service/VideoService.java
@@ -62,6 +62,18 @@ public class VideoService {
         final StringBuilder sb = new StringBuilder("{");
         final File root = new File(rootPath);
         final File videoDir = new File(root, "video");
+        final File flow = new File(videoDir, "180/" + file + "_180.mp4");
+        mediaInfo.Open(flow.getAbsolutePath());
+        sb.append("\"Duration\":").append(mediaInfo.Get(StreamKind.General, 0, "Duration")).append(",");
+        sb.append("\"DurationString\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Duration/String3")).append("\",");
+        sb.append("\"EncodedApplication\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Encoded_Application")).append("\",");
+        sb.append("\"EncodedLibrary\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Encoded_Library")).append("\",");
+        sb.append("\"InternetMediaType\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "InternetMediaType")).append("\",");
+        sb.append("\"PixelAspectRatio\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "PixelAspectRatio")).append("\",");
+        sb.append("\"DisplayAspectRatio\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "DisplayAspectRatio/String")).append("\",");
+        sb.append("\"FrameRate\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "FrameRate/String")).append("\",");
+        sb.append("\"extInfo\":[{");
+        mediaInfo.Close();
         for (VideoResolution vr : VideoResolution.values()) {
             final File resDir = new File(videoDir, vr.getResolution());
             for (File video : resDir.listFiles()) {
@@ -69,33 +81,14 @@ public class VideoService {
                     continue;
                 }
                 mediaInfo.Open(video.getAbsolutePath());
-                if (vr.equals(VideoResolution.FLOW)) {
-                    sb.append("\"Duration\":").append(mediaInfo.Get(StreamKind.General, 0, "Duration")).append(",");
-                    sb.append("\"DurationString\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Duration/String3")).append("\",");
-                    sb.append("\"EncodedApplication\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Encoded_Application")).append("\",");
-                    sb.append("\"EncodedLibrary\":\"").append(mediaInfo.Get(StreamKind.General, 0, "Encoded_Library")).append("\",");
-                    sb.append("\"InternetMediaType\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "InternetMediaType")).append("\",");
-                    sb.append("\"PixelAspectRatio\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "PixelAspectRatio")).append("\",");
-                    sb.append("\"DisplayAspectRatio\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "DisplayAspectRatio/String")).append("\",");
-                    sb.append("\"FrameRate\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "FrameRate/String")).append("\",");
-                    sb.append("\"extInfo\":[{");
-                    sb.append("\"FileName\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileName")).append(".").append(mediaInfo.Get(StreamKind.General, 0, "FileExtension")).append("\",");
-                    sb.append("\"FileSize\":").append(mediaInfo.Get(StreamKind.General, 0, "FileSize")).append(",");
-                    sb.append("\"FileSizeString\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileSize/String4")).append("\",");
-                    sb.append("\"Modified\":\"").append(mediaInfo.Get(StreamKind.General, 0, "File_Modified_Date_Local")).append("\",");
-                    sb.append("\"BitRate\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "BitRate_Nominal/String")).append("\",");
-                    sb.append("\"Width\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Width")).append("\",");
-                    sb.append("\"Height\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Height")).append("\"}");
-                } else {
-                    sb.append(",{");
-                    sb.append("\"FileName\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileName")).append(".").append(mediaInfo.Get(StreamKind.General, 0, "FileExtension")).append("\",");
-                    sb.append("\"FileSize\":").append(mediaInfo.Get(StreamKind.General, 0, "FileSize")).append(",");
-                    sb.append("\"FileSizeString\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileSize/String4")).append("\",");
-                    sb.append("\"Modified\":\"").append(mediaInfo.Get(StreamKind.General, 0, "File_Modified_Date_Local")).append("\",");
-                    sb.append("\"BitRate\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "BitRate_Nominal/String")).append("\",");
-                    sb.append("\"Width\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Width")).append("\",");
-                    sb.append("\"Height\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Height")).append("\"}");
-                }
+                sb.append(",{");
+                sb.append("\"FileName\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileName")).append(".").append(mediaInfo.Get(StreamKind.General, 0, "FileExtension")).append("\",");
+                sb.append("\"FileSize\":").append(mediaInfo.Get(StreamKind.General, 0, "FileSize")).append(",");
+                sb.append("\"FileSizeString\":\"").append(mediaInfo.Get(StreamKind.General, 0, "FileSize/String4")).append("\",");
+                sb.append("\"Modified\":\"").append(mediaInfo.Get(StreamKind.General, 0, "File_Modified_Date_Local")).append("\",");
+                sb.append("\"BitRate\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "BitRate_Nominal/String")).append("\",");
+                sb.append("\"Width\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Width")).append("\",");
+                sb.append("\"Height\":\"").append(mediaInfo.Get(StreamKind.Video, 0, "Height")).append("\"}");
                 mediaInfo.Close();
             }
         }
